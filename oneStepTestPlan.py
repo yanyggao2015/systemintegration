2b596b8ce92d135065e51b1b611bd45d4755f92c
Yan Gao <ygao@xmatters.com>
2016-08-05 15:56:06 -0700

    add oneStepTestPlan.py

---
+++ oneStepTestPlan.py
@@ -0,0 +1,42 @@
+from testrail import *
+import sys
+
+
+#Login with username and API Key
+client = APIClient('https://vic-qa-testrail.xmatters.com/testrail/')
+client.user = 'ygao'
+client.password = 'xmatters@2016'
+
+
+
+# Input release number from command line
+release = 'Web UI Service - 5.5.'+sys.argv[1]
+print 'We will create regression test plan for milestone' + release
+
+
+# Step1. Add milestone to xmatters
+print 'step 1 -- Add Milestone to project Ahch-To ......'
+result = client.send_post('add_milestone/16',
+{
+    'name' : release
+})
+milestoneid = result['id']
+
+print result['name'] 
+print milestoneid
+
+
+print 'step 2  -- Create Basic Acceptance Test Plan based on this milestone ......'
+result = client.send_post(
+	'add_plan/16',
+	{'name': 'Basic Acceptance Test Plan','milestone_id': milestoneid}
+	)
+testplanid = result['id']
+print result['name'] + ' ' + str(testplanid)
+
+print 'step 3 -- Add regression testsuites to test plan'
+
+suites = [2774, 2804]
+for suiteid in suites:
+    result = client.send_post('add_plan_entry/'+str(testplanid),{'suite_id' : suiteid,'include_all' : True})
+    print result['name']
